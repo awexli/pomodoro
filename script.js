2f6e8738ec5681d6694e44173a683b40976b7501
awexli <ali150@mail.ccsf.edu>
2020-01-01 19:22:46 -0800

    add break timers & resetTimer()

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function init () {
-  const startButton = document.getElementById('start');
-  const stopButton = document.getElementById('stop');
-  const resetButton = document.getElementById('reset');
+  const startBtn = document.getElementById('start');
+  const stopBtn = document.getElementById('stop');
+  const resetBtn = document.getElementById('reset');
+  const pomodoroBtn = document.querySelector('#pomodoro');
+  const shortBreakBtn = document.querySelector('#short-break');
+  const longBreakBtn = document.querySelector('#long-break');
   const secs = document.getElementById('seconds');
   const mins = document.getElementById('minutes');
+  let startTimer;
 
   function myTimer () {
     if (secs.innerText <= 0) {
@@ -18,34 +22,61 @@ function init () {
 
     secs.innerText = secs.innerText < 10 ? "0" + secs.innerText : secs.innerText;
      
-    // bug with clearInterval allowing start and stop to bypass this
+    // bug with clearInterval() allows spamming start and stop
+    // at the last second (ex. 00:01) to bypass this
     if (secs.innerText <= 0 && mins.innerText <= 0) {
       window.clearInterval(startTimer);
-      startButton.disabled = true;
-      stopButton.disabled = true;
+      startBtn.disabled = true;
+      stopBtn.disabled = true;
       // play alarm
     }
   }
+
+  function resetTimer (autoStart, min) {
+    if (!autoStart) {
+      window.clearInterval(startTimer)
+      startBtn.disabled = false;
+      stopBtn.disabled = false;
+    } else {
+      window.clearInterval(startTimer)
+      startTimer = setInterval(myTimer, 1000);
+      startBtn.disabled = true;
+      stopBtn.disabled = false;
+    }
+
+    secs.innerText = '0' + 0;
+    mins.innerText = min < 10 ? '0' + min : min;
+    
+  }
   
-  startButton.addEventListener('click', () => {
+  // buttons *******************************************************************
+  startBtn.addEventListener('click', () => {
     myTimer();
     startTimer = setInterval(myTimer, 1000);
-    startButton.disabled = true;
-    stopButton.disabled = false;
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
   });
 
-  stopButton.addEventListener('click', () => {
+  stopBtn.addEventListener('click', () => {
     window.clearInterval(startTimer)
-    stopButton.disabled = true;
-    startButton.disabled = false;
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
   });
 
-  resetButton.addEventListener('click', () => {
-    window.clearInterval(startTimer)
-    secs.innerText = '0' + 0;
-    mins.innerText = 25;
-    stopButton.disabled = false;
-    startButton.disabled = false;
+  resetBtn.addEventListener('click', () => {
+    resetTimer(false, 25);
+  });
+
+  pomodoroBtn.addEventListener('click', () => {
+    resetTimer(true, 25);
+  });
+
+  shortBreakBtn.addEventListener('click', () => {
+    resetTimer(true, 5);
+  });
+
+  longBreakBtn.addEventListener('click', () => {
+    resetTimer(true, 30);
   });
 }
 
