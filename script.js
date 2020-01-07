61a43785684637332898d7d4a6a8b86b1baf117a
awexli <ali150@mail.ccsf.edu>
2020-01-06 20:49:15 -0800

    complete custom timers

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function init () {
   const secs = document.querySelector('#seconds');
   const mins = document.querySelector('#minutes');
-
+  
   const startBtn = document.querySelector('#start');
   const stopBtn = document.querySelector('#stop');
   const resetBtn = document.querySelector('#reset');
@@ -23,7 +23,9 @@ function init () {
   let looping = false;
   let loops = 0;
   let count = 0;
-  let testTime = 1
+  let pomoTime = 25;
+  let shortTime = 5;
+  let longTime = 15;
 
   // ****************************** BUTTONS *********************************** // 
   startBtn.addEventListener('click', () => {
@@ -31,36 +33,27 @@ function init () {
   });
 
   stopBtn.addEventListener('click', () => {
-    window.clearInterval(startTimer)
+    window.clearInterval(startTimer);
     stopBtn.disabled = true;
     startBtn.disabled = false;
   });
 
   resetBtn.addEventListener('click', () => {
-    resetTimer(false, 25);
+    resetTimer(false, pomoTime);
     looping = false;
     enableTimerButtons();
   });
 
   pomodoroBtn.addEventListener('click', () => {
-    resetTimer(true, testTime);
+    resetTimer(true, pomoTime);
   });
 
   shortBreakBtn.addEventListener('click', () => {
-    resetTimer(true, testTime);
+    resetTimer(true, shortTime);
   });
 
   longBreakBtn.addEventListener('click', () => {
-    resetTimer(true, testTime);
-  });
-
-  incrementBtn.addEventListener('click', () => {
-    incDecMinutes(1);
-    decrementBtn.disabled = false;
-  });
-
-  decrementBtn.addEventListener('click', () => {
-    incDecMinutes(-1);
+    resetTimer(true, longTime);
   });
 
   loopBtn.addEventListener('click', () => {
@@ -70,10 +63,51 @@ function init () {
     loops = 0;
     count = 0;
 
-    resetTimer(true, testTime);
+    resetTimer(true, pomoTime);
   });
 
+  // Set custom timers
+  document.querySelectorAll('.adjust-button')
+    .forEach(element => element.addEventListener('click', e => {
+      let className = e.target.classList[1];
+      let minutes
+      let id;
+
+      if (className == 'increment') {
+        id = e.target.nextElementSibling.attributes.id.value;
+        minutes = document.getElementById(id);
+        incDecMinutes(1, minutes);
+      } else {
+        id = e.target.previousElementSibling.attributes.id.value;
+        minutes = document.getElementById(id);
+        incDecMinutes(-1, minutes);
+      }
+
+      if (id == 'pomo-mins') pomoTime = minutes.innerText;
+
+      if (id == 'short-mins') shortTime = minutes.innerText;
+
+      if (id == 'long-mins') longTime = minutes.innerText;
+
+    }));
+
   // ****************************** MAIN FUNCTIONS *********************************** // 
+  
+  /**
+   * Increments or decrements minutes
+   * @param  {number} operation 1 or -1
+   * @return {void} undefined
+   */
+  const incDecMinutes = (operation, m) => {
+    if (operation === 1) m.innerText++;
+
+    if (operation === -1) m.innerText--;
+
+    if (m.innerText < 0) m.innerText = 60;
+
+    if (m.innerText > 60) m.innerText = 0;
+  }
+
   function myTimer() {
     checkEndTimer();
 
@@ -146,7 +180,7 @@ function init () {
       enableIncDecButtons();
     } else {
       window.clearInterval(startTimer)
-      startTimer = setInterval(myTimer, 50);
+      startTimer = setInterval(myTimer, 1000);
       startBtn.disabled = true;
       stopBtn.disabled = false;
     }
@@ -196,27 +230,12 @@ function init () {
     };
   }
 
-  /**
-   * Increments or decrements minutes
-   * @param  {number} operation 1 or -1
-   * @return {void} undefined
-   */
-  const incDecMinutes = (operation) => {
-    if (operation === 1) mins.innerText++;
-
-    if (operation === -1) mins.innerText--;
-
-    if (mins.innerText < 0) mins.innerText = 60;
-
-    if (mins.innerText > 60) mins.innerText = 0;
-
-    mins.innerText = mins.innerText < 10 ? '0' + mins.innerText : mins.innerText;
-  }
-
   // ************************ ENABLE/DISABLE FUNCTIONS *************************** // 
   function disableIncDecButtons() {
-    incrementBtn.disabled = true;
-    decrementBtn.disabled = true;
+    document.querySelectorAll('.adjust-button')
+      .forEach(button => {
+        button.disabled = true;
+      })
   }
 
   function disableStartStopButtons() {
@@ -236,8 +255,10 @@ function init () {
   }
 
   function enableIncDecButtons() {
-    incrementBtn.disabled = false;
-    decrementBtn.disabled = false;
+    document.querySelectorAll('.adjust-button')
+      .forEach(button => {
+        button.disabled = false;
+      })
   }
 
   function enableTimerButtons() {
